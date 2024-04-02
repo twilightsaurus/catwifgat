be8d48644d68897131537a1a55567cb8b81cd640
Manu <tuataur@googlemail.com>
2024-04-02 16:15:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import gat from './assets/gat.png'
+
+function App() {
+
+  return (
+   <div className='wrapper'>
+    <header className='header'>
+      <img src={gat} alt="gat" className='gat' />
+    <nav>
+
+    </nav>
+
+    </header>
+   </div>
+  )
+}
+
+export default App
